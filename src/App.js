73091dd14f508232fb3cb4191b49dfced616bf2f
Praveen-Chasta <praveen.chastaa@gmail.com>
2024-10-15 20:33:30 +0530

    interlink-frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [apiData, setApiData] = useState('');
+
+  useEffect(() => {
+    const getApiData = async () => { 
+      try {
+        const response = await fetch('https://www.example.com'); 
+        const data = await response.json();
+        console.log(data);
+        setApiData(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getApiData();
+  }, []); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>API Data:</h1>
+        <pre>{JSON.stringify(apiData, null, 2)}</pre>
+      </div>
+    </>
   );
 }
 
